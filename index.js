a5daf3431870d282a662340b98577ae9166267c8
Antonio Peinado <apeinadosaucedo@gmail.com>
2023-07-24 14:12:17 +0200

    Variable

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello from external Javascritp");
+/**
+ * EXAMPLE 1 - VARIABLE declaration
+ */
+var a; 
+
+console.log(a);
+
+// console.log(b); // Uncaught ReferenceError: b is not defined
+
+let c; 
+
+console.log(c);
+
+// const d; // Uncaught SyntaxError: Missing initializer in const declaration
+
+/**
+ * example 2 - "let"
+ */
+let myNumber = 10;
+
+console.log(myNumber);
+
+let myString;
+
+myString = "Hello from String";
+
+console.log(myString);
